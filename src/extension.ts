5cc70cb503a8e02997aa041e3e82f59e1621d47d
udithv <udithv01@gmail.com>
2021-02-13 20:58:49 +0530

    added basic css

--- src/extension.ts
+++ src/extension.ts
@@ -14,9 +14,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('remotejobs.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('remotejobs.showJobs', () => {
 		// The code you place here will be executed every time your command is executed
-		RemoteJobsPanel.createOrShow(context.extensionUri)
+		RemoteJobsPanel.createOrShow(context.extensionUri);
 	});
 
 	context.subscriptions.push(disposable);

b37420b6c6fc24e291b128bb6f2eaf25a480bef4
udithv <udithv01@gmail.com>
2021-02-13 14:57:12 +0530

    created basic extension

---
+++ media/main.js
@@ -0,0 +1,35 @@
+// https://careervault.io/json/remote
+
+(function () {
+    const vscode = acquireVsCodeApi();
+    const jobs = document.getElementById("jobs");
+
+
+    fetch("https://careervault.io/json/remote")
+        .then(response => response.json())
+        .then((result) => {
+            result['data'].forEach(j => {
+                let job = document.createElement('li');
+                let jobTitle = document.createElement('h2');
+                let companyName = document.createElement('h5');
+                let location = document.createElement('p');
+                let jobLink = document.createElement('a');
+                
+                
+                companyName.innerHTML = j["CompanyName"];
+                job.append(companyName);
+
+                jobLink.href = j['Url'];
+                jobTitle.innerHTML = j["JobTitle"];
+                jobLink.append(jobTitle);
+                job.append(jobLink);
+
+                location.innerHTML = j["Location"];
+                job.append(location);
+                job.className = "job";
+
+                jobs.append(job);
+            });
+        })
+        .catch(console.log);
+}());
